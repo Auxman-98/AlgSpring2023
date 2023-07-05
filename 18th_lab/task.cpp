82adea96ae6955be954db331b9feabe510da2895
Auxman-98 <138567488+Auxman-98@users.noreply.github.com>
2023-07-05 19:53:31 +0200

    Update task.cpp

--- 18th_lab/task.cpp
+++ 18th_lab/task.cpp
@@ -1,81 +1,38 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
-
-void smartReset(double* array, int n) // ввод элементов множества и его проверка на повторяющиеся элементы
-{
-	setlocale(LC_ALL, "Russian");
-	for (int i = 0; i < n; i++) {
-		cout << "Введите " << (i) << " элемент: ";
-		cin >> array[i];
-		bool flag = false; // повторяющихся элементов нет по умолчанию
-		for (int j = 0; j < i; j++) // проверка на повтор
-		{
-			if (array[i] == array[j]){
-				flag = true;
-				break;
-			}
-		}
-		if (flag) // если повтор был
-		{
-			cout << "Такой элемент уже есть в множестве" << endl;
-			i--;
-		}
-	}
-}
-
-void subsetWithSum(double* array, int n, double sum) // проверяем наличие подмножества с заданной суммой
-{
-	int ires = -1;
-	int k = powf(2, n); // количество подмножеств
-	for (int i = 0; i < k; i++) // перебор подмножеств
-	{
-		int partial = 0;
-		bool flag = false; // искомое подмножество пусто по умолчанию
-		for (int j = 0; j < n; j++)
-			if (i & (1 << j)){
-				partial += array[j]; // набираем сумму
-				flag = true; // подмножество непустое
-			}
-		if (partial == sum && flag) {
-			ires = i;
-			break; // с искомым подмножеством выходим из перебора
-		}
-	}
-
-	if (ires == -1)
-		cout << "Подмножество с заданной суммой не существует" << endl;
-	else {
-		cout << "Подмножество с заданной суммой:\n";
-		for (int j = 0; j < n; j++)
-			if (ires & (1 << j))
-				cout << array[j] << " ";
-		cout << endl; // выходим из цикла
-	}
-}
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	double* a;
-	double s;
 	int n;
-	do {
-		cout << "Введите размер множества: ";
-		cin >> n;
-	} while (n < 1); // проверка на допустимое значение n
-
-	a = new double[n]; // выделяем память
-
-	smartReset(a, n);
-
-	cout << "Введите нужную сумму чисел: ";
-	cin >> s;
-
-	subsetWithSum(a, n, s);
-
-	delete[] a; // освобождаем память
+	cout << "Р’РІРµРґРёС‚Рµ РјРѕС‰РЅРѕСЃС‚СЊ РјРЅРѕР¶РµСЃС‚РІР°: ";
+	cin >> n;
+	cout << endl;
+	vector<int> set(n);
+	cout << "Р’РІРµРґРёС‚Рµ СЌР»РµРјРµРЅС‚С‹ РјРЅРѕР¶РµСЃС‚РІР°:";
+	for (int i = 0; i < n; i++){
+		cin >> set[i];
+	}
+	sort(set.begin(), set.end()); // СЃРѕСЂС‚РёСЂРѕРІРєР° РјР°СЃСЃРёРІР°
+	sum = array[0]; // РЅР°С‡Р°Р»СЊРЅР°СЏ СЃСѓРјРјР°
+	vector<int> subset = { set[0] }; // РЅР°С‡Р°Р»СЊРЅРѕРµ РїРѕРґРјРЅРѕР¶РµСЃС‚РІРѕ
+	for (int i = 1; i < set.size(); i++) {
+            if (sum + set[i] >= set[i]) { // РїСЂРѕРІРµСЂРєР° СѓСЃР»РѕРІРёСЏ РЅР° РІРєР»СЋС‡РµРЅРёРµ СЌР»РµРјРµРЅС‚Р° РІ РїРѕРґРјРЅРѕР¶РµСЃС‚РІРѕ
+                sum += set[i];
+                subset.push_back(set[i]);
+            }
+        }
+	if (subset = { 0 }){
+		cout << "РџРѕРґРјРЅРѕР¶РµСЃС‚РІР° СЃ Р·Р°РґР°РЅРЅРѕР№ СЃСѓРјРјРѕР№ РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚!" << endl;
+		return 0;
+	}
+	cout << "РЎСѓРјРјР° РЅР°РёР±РѕР»СЊС€РµРіРѕ РїРѕРґРјРЅРѕР¶РµСЃС‚РІР°: " << sum << endl;
+	for (int i = 0; i < subset.size(); i++) {
+                cout << subset[i] << " "; // РІС‹РІРѕРґ СЌР»РµРјРµРЅС‚РѕРІ РїРѕРґРјРЅРѕР¶РµСЃС‚РІР°
+        }
+        cout << endl;
 
 	return 0;
 }
