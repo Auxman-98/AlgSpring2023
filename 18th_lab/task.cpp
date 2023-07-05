1751bf752147ded8fe2d66236ca6a4ef17b9c69d
Auxman-98 <138567488+Auxman-98@users.noreply.github.com>
2023-07-05 22:20:11 +0200

    Add files via upload

---
+++ 18th_lab/task.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int arr[101];
+
+void printMask(int mask, int n)
+{
+	for (int i = 0; i < n; i++) {
+		if (mask % 2)
+			cout << arr[i] << " ";
+		mask /= 2;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cout << "¬ведите мощность множества: ";
+	cin >> n;
+	cout << "¬ведите элементы множества:" << endl;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	int t;
+	cout << "¬ведите нужную сумму подмножества: ";
+	cin >> t;
+	for (int mask = 1; mask < (1 << n); mask++)
+	{
+		int tmp = mask, curSum = 0;
+		for (int i = 0; i < n; i++) {
+			if (tmp % 2)
+				curSum += arr[i];
+			tmp /= 2;
+		}
+		if (curSum == t) {
+			cout << "Ёлементы подмножества с заданной суммой: ";
+			printMask(mask, n);
+			return 0;
+		}
+	}
+	cout << "ѕодмножества с суммой t не существует" << endl;
+	
+	return 0;
+}
